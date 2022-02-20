0c4726f94a7a4f6fa85338597bfcb670eb769e08
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-02-20 10:37:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p><b>WikiNotes</b> project</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          あなたの好きをWikiNotesでもっと探索しよう 🧭
+        </p>
+        <a
+          className="App-link"
+          href="https://vividarmy-wikinotes.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          VividArmy WikiNotes
+        </a>
       </header>
     </div>
   );
